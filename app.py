dc237fbd74282f6ecca63f855f07e3ff2aa2f5c7
Darkangel7182 <95593401+Darkangel7182@users.noreply.github.com>
2026-02-06 03:20:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 import os
 import random
+import time
 from flask import Flask, request
 from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'fenerbahce1907'
+app.config['SECRET_KEY'] = 'fenerbahce1907' #
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 odalar = {}
-HARFLER = "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ"
+HARFLER = "ABCÇDEFGĞHİIJKLMNOÖPRSŞTUÜVYZ" #
 
 @socketio.on('oda_olustur')
 def handle_create(data):
     oda = data['oda']
     sifre = data['sifre']
+    # Odayı her zaman tazeleyerek oluştur (Çakışma önleyici)
     odalar[oda] = {
         'sifre': sifre,
         'host': request.sid,
         'kategoriler': ["İsim", "Şehir", "Hayvan"],
         'harf': '',
         'cevaplar': {},
-        'puanlandi': False # Puanlamanın mükerrer olmasını engellemek için
+        'puanlandi': False
     }
     join_room(oda)
     emit('oda_katildi', {'oda': oda, 'is_host': True})
@@ -35,13 +37,6 @@ def handle_join(data):
         emit('kategorileri_guncelle', {'kategoriler': odalar[oda]['kategoriler']}, room=oda)
     else:
         emit('hata', {'mesaj': 'Oda adı veya şifre hatalı!'})
-
-@socketio.on('kategori_degistir')
-def handle_category_change(data):
-    oda = data['oda']
-    if oda in odalar and odalar[oda]['host'] == request.sid:
-        odalar[oda]['kategoriler'] = data['kategoriler']
-        emit('kategorileri_guncelle', {'kategoriler': odalar[oda]['kategoriler']}, room=oda)
 
 @socketio.on('oyunu_baslat')
 def handle_start(data):
@@ -58,20 +53,18 @@ def handle_start(data):
 @socketio.on('oyunu_bitir')
 def handle_finish(data):
     oda = data['oda']
-    if oda in odalar:
-        emit('geri_sayim_baslat', {'sure': 10}, room=oda)
-        # 12 saniye bekle (10 saniye oyun + 2 saniye ağ gecikmesi)
-        socketio.sleep(12) 
-        if not odalar[oda]['puanlandi']:
-            puanla(oda)
-            odalar[oda]['puanlandi'] = True
+    emit('geri_sayim_baslat', {'sure': 10}, room=oda)
+    # Herkesin cevabını göndermesi için 12 saniyelik güvenli süre
+    socketio.sleep(12)
+    if oda in odalar and not odalar[oda]['puanlandi']:
+        puanla(oda)
+        odalar[oda]['puanlandi'] = True
 
 @socketio.on('cevaplari_gonder')
 def handle_answers(data):
     oda = data['oda']
     if oda in odalar:
         odalar[oda]['cevaplar'][request.sid] = data['cevaplar']
-        # Burada artık puanla(oda) çağırmıyoruz, zamanlayıcıyı bekliyoruz
 
 def puanla(oda):
     sonuclar = {}
@@ -86,6 +79,7 @@ def puanla(oda):
             kelime = cevaplar.get(kat, "").strip().upper()
             puan = 0
             if kelime and kelime.startswith(harf):
+                # Aynı cevabı verenleri kontrol et
                 digerleri = [v.get(kat, "").strip().upper() for k, v in cevaplar_havuzu.items() if k != oyuncu]
                 puan = 5 if kelime in digerleri else 10
             toplam += puan
@@ -94,4 +88,5 @@ def puanla(oda):
     emit('puan_durumu', sonuclar, room=oda)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
